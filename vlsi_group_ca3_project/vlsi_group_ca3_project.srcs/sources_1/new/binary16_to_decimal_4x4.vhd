a19669fd94681126fa43827e93bd5b93cec450b3
Harmony <harmonised7@gmail.com>
2023-03-31 17:35:58 +0100

    Potato

--- vlsi_group_ca3_project/vlsi_group_ca3_project.srcs/sources_1/new/binary16_to_decimal_4x4.vhd
+++ vlsi_group_ca3_project/vlsi_group_ca3_project.srcs/sources_1/new/binary16_to_decimal_4x4.vhd
@@ -11,21 +11,23 @@ port
 end entity binary16_to_decimal_4x4;
 
 architecture rtl of binary16_to_decimal_4x4 is
+signal s_output_decimal:std_logic_vector(15 downto 0) := b"0000000000000000";
 begin
 
 process(input_binary)
-        variable temp_num : integer range 0 to 9999 := input_binary;
-        variable digit : integer;
-        variable quotient : integer;
-        variable divisor : integer := 1000;
-    begin
-        for i in 0 to 3 loop
-            digit := temp_num / divisor;
-            quotient := temp_num - digit * divisor;
-            temp_num := quotient;
-            divisor := divisor / 10;
-            output_decimal(i*4+3 downto i*4) <= std_logic_vector(to_unsigned(digit, 4));
-        end loop;
-    end process;
+    variable temp_num : integer range 0 to 9999 := input_binary;
+    variable digit : integer;
+    variable quotient : integer;
+    variable divisor : integer := 1000;
+begin
+    for i in 0 to 3 loop
+        digit := temp_num / divisor; 
+        quotient := temp_num - digit * divisor;
+        temp_num := quotient;
+        divisor := divisor / 10;
+        s_output_decimal(i*4+3 downto i*4) <= std_logic_vector(to_signed(digit, 4));
+    end loop;
+end process;
+output_decimal <= s_output_decimal;
 
 end architecture rtl;
